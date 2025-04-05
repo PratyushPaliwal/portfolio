9ca863e7b9dcc2666f3c16845796a950224d3934
PratyushPaliwal <paliwalpratyush1510@email.com>
2025-04-05 16:30:46 +0200

    update

--- src/components/base-image.tsx
+++ src/components/base-image.tsx
@@ -2,21 +2,21 @@
 "use client";
 
 import Image, { ImageProps } from 'next/image';
-import { usePathname } from 'next/navigation';
 
 interface BaseImageProps extends Omit<ImageProps, 'src'> {
   src: string;
+  alt: string; // Ensure alt is required
 }
 
-export default function BaseImage({ src, ...props }: BaseImageProps) {
-  const pathname = usePathname();
+export default function BaseImage({ src, alt, ...props }: BaseImageProps) {
+  // We removed the unused pathname variable
   const isLocalImage = src.startsWith('/') && !src.startsWith('//') && !src.startsWith('http');
   
   // For external images, use the original src
   if (!isLocalImage) {
-    return <Image src={src} {...props} />;
+    return <Image src={src} alt={alt} {...props} />;
   }
   
   // For local images, make sure they work in both development and production
-  return <Image src={src} {...props} />;
+  return <Image src={src} alt={alt} {...props} />;
 }
