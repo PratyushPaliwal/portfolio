9df7d83a5a6db5e3f0c5adab81239a20053b9c9a
PratyushPaliwal <paliwalpratyush1510@email.com>
2025-04-05 16:43:20 +0200

    adding '/portfolio' for image in gh pages

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -35,7 +35,7 @@ export const aboutMe: AboutMe = {
     // "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   // googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
   // githubUsername: "janesmith", // You might want to update this to your actual GitHub username
-  imageUrl: "/linkedinphoto.png",
+  imageUrl: "portfolio/linkedinphoto.png",
   linkedinUsername: "paliwalpratyush",
   twitterUsername: "pratyushpa1iwal",
   blogUrl: "https://", // Update or remove if you don't have a blog
